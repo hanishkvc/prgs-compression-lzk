6df0fccb114647e88b314946762578cc99a5b53a
hanishkvc <hanishkvc@gmail.com>
2018-02-08 13:07:13 +0000

    Converted the compress logic into a function

--- lzk.py
+++ lzk.py
@@ -5,33 +5,36 @@ import enum
 
 P = enum.Enum('P', 'START SEARCH')
 
+
+def compress(l):
+	d = dict()
+	diStart = di = 128
+	state = P.START
+	for i in range(len(l)):
+		if (state == P.START):
+			c = l[i]
+			state = P.SEARCH
+			partFound = None
+		elif (state == P.SEARCH):
+			c += l[i]
+			found = None
+			for j in range(diStart, di):
+				if d[j] == c:
+					partFound = j
+					found = j
+					break
+			if (found == None):
+				d[di] = c
+				di += 1
+				if partFound == None:
+					partFound = l[i-1]
+					partFound = c[0]
+				print("{}, {}".format(partFound, l[i]))
+				state = P.START
+	print(d)
+
+
+
 l = sys.argv[1]
-
-d = dict()
-diStart = di = 128
-state = P.START
-for i in range(len(l)):
-	if (state == P.START):
-		c = l[i]
-		state = P.SEARCH
-		partFound = None
-	elif (state == P.SEARCH):
-		c += l[i]
-		found = None
-		for j in range(diStart, di):
-			if d[j] == c:
-				partFound = j
-				found = j
-				break
-		if (found == None):
-			d[di] = c
-			di += 1
-			if partFound == None:
-				partFound = l[i-1]
-				partFound = c[0]
-			print("{}, {}".format(partFound, l[i]))
-			state = P.START
-
-print(d)
-
+compress(l)
 
