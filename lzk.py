f41e83ad3c95c5e2123af1f3f77a4a688c507f95
hanishkvc <hanishkvc@gmail.com>
2018-02-09 07:43:39 +0000

    Initial overcomplicated decompress with corner cases
    By trying to reuse the compress logic for decompress also, have
    created a unnecessary over complicated monster. Which also leads
    to corner cases of bugs.

--- lzk.py
+++ lzk.py
@@ -46,10 +46,49 @@ def compress(l, diStart = 256):
 	return [d, l, lc]
 
 
+def decompress(l, diStart = 256):
+	ld = bytearray()
+	d = dict()
+	di = diStart
+	state = P.START
+	for i in range(len(l)):
+		if (state == P.START):
+			c = bytearray()
+			c.append(l[i])
+			state = P.SEARCH
+			partFound = None
+		elif (state == P.SEARCH):
+			c.append(l[i])
+			if l[i] < diStart:
+				found = None
+			else:
+				found = l[i]
+				partFound = l[i]
+			if (found == None):
+				d[di] = c
+				di += 1
+				if partFound == None:
+					#partFound = l[i-1]
+					partFound = c[0]
+					if partFound >= diStart:
+						partFound = d[partFound]
+				else:
+					partFound = d[partFound]
+				print("{}, {}".format(partFound, l[i]))
+				if (type(partFound) == type(int())):
+					ld.append(partFound)
+				else:
+					ld.extend(partFound)
+				ld.append(l[i])
+				state = P.START
+	print(d)
+	dprint("{}, [{}]".format(len(l), l))
+	dprint("{}, [{}]".format(len(ld), ld))
+	return [d, l, ld]
+
 
 def compress_str(l, diStart=128):
 	return compress(bytes(l,'ascii'), diStart)
-
 
 
 def decompress_easy(dOrig, l, diStart=256):
@@ -80,5 +119,7 @@ l = sys.argv[1]
 [d, lc, ld] = decompress_easy(d, lc, 128)
 print(l)
 print(lc)
+print(ld)
+[d, lc, ld] = decompress(lc, 128)
 print(ld)
 
